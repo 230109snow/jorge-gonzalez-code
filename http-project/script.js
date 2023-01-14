7dab665fbbbc62171103bbcebf800625f03c3179
Jorge Gonzalez <jglez.cs@gmail.com>
2023-01-13 21:12:17 -0600

    Takes in user text and fetches user data and updates DOM

--- http-project/script.js
+++ http-project/script.js
@@ -1,5 +1,10 @@
 const searchBar = document.querySelector("input");
 const form = document.querySelector("form");
+const nameElement = document.querySelector("#dev-name");
+const usernameElement = document.querySelector("#username");
+const bioElement = document.querySelector("#bio");
+const joinElement = document.querySelector("#join-date");
+const imgElement = document.querySelector("img");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -14,9 +19,31 @@ function getUserData(username) {
   .then(res => {
     res.json()
     .then(data => {
-      console.log("Name:", data.name)
-      console.log("GitHub username:", data.login);
+
+      const joinDate = {
+        year: data.created_at.substring(0, 4),
+        month: data.created_at.substring(5, 7),
+        day: data.created_at.substring(8, 10)
+      }
+
+      updateUserInfo(
+        data.name,
+        data.login,
+        data.bio,
+        joinDate,
+        data.avatar_url
+      );
+
     })
   })
   
+}
+
+
+function updateUserInfo(name, username, bio, joinDate, src) {
+  nameElement.innerText = name;
+  usernameElement.innerText = username;
+  bioElement.innerText = bio;
+  joinElement.innerText = `Joined ${joinDate.month}/${joinDate.day}/${joinDate.year}`;
+  imgElement.src = src;
 }
