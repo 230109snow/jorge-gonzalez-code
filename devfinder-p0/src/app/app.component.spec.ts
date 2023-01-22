da9b0eff2034761d966c76188f612cfc7f969188
Jorge Gonzalez <jglez.cs@gmail.com>
2023-01-22 14:45:13 -0600

    Adds SearchbarComponent files, adds Space Mono typeface

--- devfinder-p0/src/app/app.component.spec.ts
+++ devfinder-p0/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'github-search-p0'`, () => {
+  it(`should have as title 'devFinder'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('github-search-p0');
+    expect(app.title).toEqual('devFinder');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('github-search-p0 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('devFinder app is running!');
   });
 });
